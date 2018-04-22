64a1b6de97b4e9eaaf08fee4a91824bd17a85199
archangel189 <archanshell@gmail.com>
2018-04-22 18:20:45 +0100

    Add solution to Contest 964 - Problem A

---
+++ contest/964/problem-a.go
@@ -0,0 +1,15 @@
+/*
+   Solution to Contest 964 - Problem A.
+   http://codeforces.com/contest/964/problem/A
+*/
+
+package main
+
+import "fmt"
+
+var x int
+
+func main() {
+	fmt.Scanf("%d", &x)
+	fmt.Println(x/2 + 1)
+}
